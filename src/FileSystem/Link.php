b589e04d588d1cee0718261d71a8fa5848c80a96
STEIN197 <nil20122013@gmail.com>
2020-06-30 21:38:00 +0500

    Created constructor body

--- src/FileSystem/Link.php
+++ src/FileSystem/Link.php
@@ -6,7 +6,12 @@
 		public const LINK_SOFT = 0;
 		public const LINK_HARD = 1;
 
-		public function __construct(string $path, int $resolution = Path::PATH_CWD, ?File $file = null) {} // TODO
+		public function __construct(string $path, int $resolution = Path::PATH_CWD, ?File $file = null) {
+			$this->path = new Path($path, $resolution);
+			if ($this->exists() && !is_link($this->path->getAbsolute()))
+				throw new LogicException("Cannot instantiate link class: '{$this}' is not file");
+		}
+
 		public function link(Descriptor $file, int $type = self::LINK_SOFT): ?Descriptor {} // TODO
 		public function read(): ?Descriptor {} // TODO
 	}
